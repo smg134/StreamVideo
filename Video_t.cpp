b2a52aca5a962cdb246895f5d9b8c7f7e66291d4
rakegi <rayginesk@gmail.com>
2017-04-30 11:18:26 -0400

    Use setLength to set hours and minutes and return new values

--- Video_t.cpp
+++ Video_t.cpp
@@ -29,5 +29,14 @@ int main() {
     assert(myVideo.getEpisodes() == 0);
   }
   
+  //Use setLength to set hours and minutes and return new values
+  {
+    Video myVideo("", 0, 0, 0, 0);
+    
+    myVideo.setLength(3, 7);
+    assert(myVideo.getHours() == 3);
+    assert(myVideo.getMinutes() == 7);
+  }
+  
   return 0;
 }
