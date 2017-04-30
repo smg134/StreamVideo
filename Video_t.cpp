bc1fafc5ff4f51a1d6cda2ccb131c018af47cbf4
rakegi <rayginesk@gmail.com>
2017-04-30 11:08:38 -0400

    Test added: initalize constructor with default values and use getter functions to return values

--- Video_t.cpp
+++ Video_t.cpp
@@ -11,8 +11,22 @@
 int main() {
 
   // TODO: Add test cases
+  
+  //Empty default constructor initializes
   {
     Video myVideo();
+  }
+  
+  //Initalize a constuctor with default values and
+  //use the getter functions to return those values.
+  {
+    Video myVideo("", 0, 0, 0, 0);
+    
+    assert(myVideo.getTitle() == "");
+    assert(myVideo.getType() == 0);
+    assert(myVideo.getHours() == 0);
+    assert(myVideo.getMinutes() == 0);
+    assert(myVideo.getEpisodes() == 0);
   }
   
   return 0;
