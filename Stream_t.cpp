7719a1b56f9e147131784124a41db4286cc966d0
Kevin Lewis <kevlew02@gmail.com>
2017-05-03 18:03:01 -0400

    Added test cases for Stream class

--- Stream_t.cpp
+++ Stream_t.cpp
@@ -10,7 +10,24 @@
 
 int main() {
 
-    // TODO: Add test cases
+	//Initialize empty stream object
+    {Stream myStream();}
+
+
+    //Test stream constructor with specific values
+    //and assert those values with the corresponding
+    //getter methods
+    {
+    const Video myVideo("video", 2, 0, 0, 2);
+    Stream myStream(myVideo, 1);
+
+    assert(myStream.getOccurrences() == 1);
+
+    assert(myStream.getVideo().getTitle() == myVideo.getTitle());
+    assert(myStream.getVideo().getType() == myVideo.getType());
+    assert(myStream.getVideo().getHours() == myVideo.getHours());
+    assert(myStream.getVideo().getMinutes() == myVideo.getMinutes());
+	}
 
     return 0;
 }
