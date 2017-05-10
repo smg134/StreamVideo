5a53d4658b80e7a073f0dcb75b88ee76d9b48e3c
Sean Gaffney <smg134@zips.uakron.edu>
2017-05-10 17:52:38 -0400

    Added individual test cases for each stream type

--- Account_t.cpp
+++ Account_t.cpp
@@ -21,8 +21,40 @@ int main() {
         // test using literal string
         assert(customer.data() == "");
     }
-
-    // one stream
+	
+	// MOVIE stream
+	{	
+		Video myVideo("movie", 0, 2, 10, 0);
+		Stream myStream(myVideo, 1);
+		
+		Account customer("Fred");
+		
+		customer.addStream(myStream);
+		
+		assert(customer.getName() == "Fred");
+	
+		assert(customer.data() == "Fred,MOVIE,movie,2,10,2\n");
+		
+		assert(customer.report() == "Stream Report for Account: Fred\nStreams:\n\tmovie\t2\n\nTotal Stream Events: 2\nNon-Original Stream Events: 2\nTotal Time: 2:10\n");
+	}
+    
+	// TVSHOW stream
+	{	
+		Video myVideo("tv_show", 1, 4, 6, 12);
+		Stream myStream(myVideo, 1);
+		
+		Account customer("Fred");
+		
+		customer.addStream(myStream);
+		
+		assert(customer.getName() == "Fred");
+	
+		assert(customer.data() == "Fred,TVSHOW,tv_show,4,6,1\n");
+		
+		assert(customer.report() == "Stream Report for Account: Fred\nStreams:\n\ttv_show\t1\n\nTotal Stream Events: 1\nNon-Original Stream Events: 1\nTotal Time: 4:6\n");
+	}
+	
+	// ORIGINAL stream
 	{	
 		Video myVideo("video", 2, 0, 0, 2);
 		Stream myStream(myVideo, 1);
@@ -37,7 +69,7 @@ int main() {
 		
 		assert(customer.report() == "Stream Report for Account: Fred\nStreams:\n\tvideo\t1\n\nTotal Stream Events: 1\nNon-Original Stream Events: 0\nTotal Time: 0:0\n");
 	}
-    
+	
 	//multiple streams
 	{	
 		Video myOriginal("original", 2, 0, 30, 1);
@@ -61,7 +93,8 @@ int main() {
 
 		assert(customer.report() == "Stream Report for Account: Fred\nStreams:\n\toriginal\t1\n\ttvshow\t1\n\tmovie\t2\n\n"
 			"Total Stream Events: 4\nNon-Original Stream Events: 3\nTotal Time: 3:30\n");
-	}
+}
 	
+	// 
     return 0;
 }
